f6d773c4e6143e2ecd66bee8a815c1b41d82fff4
Okall Vivian <vivieokall@gmail.com>
2019-03-26 00:12:58 +0300

    "Add packages"

--- neighbor/models.py
+++ neighbor/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from smartfields import fields
+from django.utils import timezone
+from django.conf import settings
 
 # Create your models here.
+class Hood(models.Model):
+    name = models.CharField(max_length =30,null=True)
+    image = models.ImageField(upload_to = 'images/')
+    description = models.TextField(null=True)
+    link = models.URLField()
+    user = models.ForeignKey(User, null=True,on_delete=models.CASCADE)
+    location = models.CharField(max_length =30,null=True)
+    occupants = models.IntegerField(null=True)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+    def save_hood(self):
+        self.save()
+
+    def delete_hood(self):
+        self.delete()
+
+class Profile(models.Model):
+    """User profile data."""
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    dob = models.DateTimeField(blank=True, null=True)
+    bio = models.CharField(max_length=140, blank=True, null=True)
+    avatar = fields.ImageField(upload_to='uploads/')
+    hood = models.CharField(max_length=40, blank=True, null=True)
+    hobby = models.CharField(max_length=40, blank=True, null=True)
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
--- neighbor/views.py
+++ neighbor/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from .forms import SignupForm, HoodForm, UserProfileUpdateForm, UserUpdateForm 
+from .models import *
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
+@login_required(login_url="/accounts/login/")
+def home(request):
+    hoods = Hood.objects.all()
+    
+    return render(request,"home.html",locals())
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = True
+            user.save()
+            return render(request, 'home.html')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
+
+def new_hood(request):
+    current_user = request.user
+    if request.method == "POST":
+        form = HoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            hood = form.save(commit=False)
+            hood.user = current_user
+            hood.save()
+        return redirect("home")
+
+    else:
+        form = HoodForm()
+    return render(request, "new_hood.html", {"form": form})
+
+@login_required
+def profile(request):
+    """Display user profile information."""
+    user = request.user
+    return render(request, 'profile.html', {'user': user})
+
+@login_required
+def update_profile(request):
+    """Edit user profile information."""
+    user = request.user
+    form1 = UserUpdateForm(instance=user)
+    form2 = UserProfileUpdateForm(instance=user.profile)
+    if request.method == 'POST':
+        form1 = UserUpdateForm(instance=user, data=request.POST)
+        form2 = UserProfileUpdateForm(
+            instance=user,
+            data=request.POST,
+            files=request.FILES
+        )
+        if form1.is_valid() and form2.is_valid():
+            form1.save()
+            form2.save()
+            messages.success(request, "Your profile has been updated!")
+            return HttpResponseRedirect(reverse('profile'))
+    return render(request, 'update_profile.html',
+        {'form1': form1, 'form2': form2})
