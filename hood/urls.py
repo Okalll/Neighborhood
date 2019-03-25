6edd52e5bd716db93e34a6b768f5cb7d8ee41ee9
Okall Vivian <vivieokall@gmail.com>
2019-03-25 19:08:33 +0300

    update urls

--- hood/urls.py
+++ hood/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import url,include
+from django.conf.urls import url, include
 from django.contrib.auth import views
 
 urlpatterns = [
+    url(r'',include('neighbor.urls')),
     url('admin/', admin.site.urls),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
     url(r'^accounts/', include('registration.backends.simple.urls')),
